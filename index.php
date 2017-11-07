0f71c51dd569b75d3d6db430c9cc2975413ce8a6
andxz <andrez.romero@hotmail.com>
2017-11-07 11:10:05 +0100

    testar lite

--- index.php
+++ index.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Millhouse</title>
+
+<link rel="stylesheet" href="https://cdn.rawgit.com/tonsky/FiraCode/1.204/distr/fira_code.css">
+
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
+<header>
+
+
+Bild
+
+
+</header>
 <body>
     
     
-    <h1> Test </h1>
+    <h1> Body </h1>
+
+test
+
+
+
 
 
 </body>
