092fe87f25093e03ded994f69a3eedd6e6a8cf0a
vctrklndr <victor@nasa.se>
2017-11-07 10:54:48 +0100

    Added Fira Code to index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Millhouse</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/tonsky/FiraCode/1.204/distr/fira_code.css">
 </head>
 <body>
     
