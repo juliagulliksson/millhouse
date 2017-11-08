9df11eabad551aa8a2cb2b1aa99610a47c269a46
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-11-08 19:44:24 +0100

    Last commit of the day

--- partials/sql.php
+++ partials/sql.php
@@ -2,7 +2,9 @@
 
 require 'database.php';
 
-
+// NÄR  GET-request för varje id är set, på undersidorna för alla blogposts, så hämtas kommentarerna via
+// en sql query WHERE comments.post_id = $id(idt som har skickats med i GET)
+//Men själva kommentarerna måste ju också räknas... för att kunna visas upp i comments(2) 
 $statement = $pdo->prepare("SELECT posts.date, posts.text, posts.post_title, posts.date, categories.title, users.username
 FROM posts 
 INNER JOIN categories 
