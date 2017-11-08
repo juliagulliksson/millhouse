92b7505c54c4e84d1079579d64fb1bbfa9fba55e
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-11-08 16:51:31 +0100

    Adding blog posts

--- partials/insert.php
+++ partials/insert.php
@@ -2,17 +2,21 @@
 <?php
 require 'database.php';
 
-//header('location: ../index.php');
+header('location: ../index.php');
 
 var_dump($_POST);
 $title = $_POST['blog_title'];
 $category = $_POST['category'];
 $body = $_POST['post_text'];
-//$date = date('Y-n-d');
+$date = date('Y-n-d');
+echo $title;
+echo $category;
+echo $body;
+echo $date;
 //echo $date;
 
-$statement = $pdo->prepare("INSERT INTO posts (post_title) 
-VALUES ($title)");
+$statement = $pdo->prepare("INSERT INTO posts (post_title, category_id, text) 
+VALUES ('$title', $category, '$body')");
 $statement->execute();
   
   /*$statement->execute(array(
