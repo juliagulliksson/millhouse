d6f55893359d6db6c930ce39abd4cf6e59daee0d
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-11-16 16:19:58 +0100

    Fixed blog post insert

--- partials/insert.php
+++ partials/insert.php
@@ -2,13 +2,14 @@
 header('location: ../index.php');
 require 'database.php';
 
+
 $title = $_POST['blog_title'];
 $category = $_POST['category'];
 $body = $_POST['post_text'];
 
 $user_id = $_POST['user_id'];
 
-var_dump($_POST);
+
 $statement = $pdo->prepare("INSERT INTO posts (post_title, category_id, text, date, user_id) 
 VALUES ('$title', $category, '$body', CURRENT_TIMESTAMP(), $user_id)");
 $statement->execute();
