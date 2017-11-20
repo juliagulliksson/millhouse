82f451294e9faa2508a5546ca9780f6754bb564b
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-11-20 12:41:42 +0100

    Renamed functions.php, moved to /functions

--- profile_includes/profile_sql.php
+++ profile_includes/profile_sql.php
@@ -3,35 +3,43 @@ require './partials/database.php';
 $id = $_SESSION['id'];
 
 //Fetch the number of posts made by the user
-$statement = $pdo->prepare("SELECT COUNT(posts.id) as posts 
+$statement = $pdo->prepare("SELECT COUNT(posts.id) as number_of_posts 
 FROM posts
-WHERE user_id = '$id'
+WHERE user_id = :id
 ");
-$statement->execute();
+$statement->execute(array(
+    ":id" => $id
+));
 $profile_articles = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 //Fetch the number of comments made by the user
-$statement = $pdo->prepare("SELECT COUNT(comments.id) as comments 
+$statement = $pdo->prepare("SELECT COUNT(comments.id) as number_of_comments 
 FROM comments
-WHERE user_id = '$id'
+WHERE user_id = :id
 ");
-$statement->execute();
+$statement->execute(array(
+    ":id" => $id
+));
 $profile_comments = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 //Fetch all blogposts made by the user, limit to 5
 $statement = $pdo->prepare("SELECT * FROM posts 
-WHERE user_id = '$id'
+WHERE user_id = :id
 LIMIT 5
 ");
-$statement->execute();
+$statement->execute(array(
+    ":id" => $id
+));
 $profile_blogposts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 //Fetch all user comments made by the user, limit to 5
 $statement = $pdo->prepare("SELECT * FROM comments 
-WHERE user_id = '$id'
+WHERE user_id = :id
 LIMIT 5
 ");
-$statement->execute();
+$statement->execute(array(
+    ":id" => $id
+));
 $profile_comments_title = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 //Fetch all blogposts made by the user
@@ -44,8 +52,10 @@ ON posts.category_id=categories.id
 INNER JOIN users
 ON posts.user_id=users.id
 LEFT JOIN comments ON posts.id=comments.post_id
-WHERE posts.user_id = '$id'
+WHERE posts.user_id = :id
 GROUP BY posts.id
 ORDER BY posts.date DESC");
-$statement->execute();
+$statement->execute(array(
+    ":id" => $id
+));
 $profile_all_articles = $statement->fetchAll(PDO::FETCH_ASSOC);
