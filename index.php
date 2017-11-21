a5a7f4098385b082bd714e29fab9348e8c7d4743
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-11-21 17:40:40 +0100

    saved

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 require 'partials/head.php';
 ?>
-
     <?php
     // Startpage blog posts
     if(!isset($_GET['id']) && !isset($_GET['category']) 
@@ -11,17 +10,13 @@ require 'partials/head.php';
             include 'partials/blog_posts.php';
         endforeach;
     endif;
-
     // Individual blog posts
     if(isset($_GET['id'])):
         require 'partials/article_singles.php';
     endif; //END OF GET ID IF 
-
     if(isset($_GET['category']) && !isset($_GET['asc'])):
         $categories = $_GET['category'];
         include 'actions/category_articles.php';  
-
-
     if (count($category_articles) > 1):
     ?>  
         <h4>Sort by date | <a name="newest" href="index.php?category=<?= $categories?>
@@ -29,27 +24,21 @@ require 'partials/head.php';
         
     <?php 
     endif;//End of count category_articles if
-
     foreach($category_articles as $article):
         include 'partials/blog_posts.php';    
     endforeach;       
-
         
     elseif(isset($_GET['category']) && isset($_GET['asc'])): //ascending categories
         $categories = $_GET['category'];
         include 'partials/category_articles.php';
         ?>
-
     <h4>Sort by date | <a name="oldest" href="index.php?category=<?= $categories?>
     #newest">Newest</a></h4>
-
         <?php
         foreach($category_articles_asc as $article):
             include 'partials/blog_posts.php';
         endforeach; 
     endif; //END OF CATEGORIES 
-
-
     if(isset($_GET['month'])):
         $month = $_GET['month'];
         include 'actions/month_articles.php';
@@ -58,7 +47,6 @@ require 'partials/head.php';
             include 'partials/blog_posts.php';
         endforeach; 
     endif; //END OF MONTHS 
-
             if(isset($_GET['month'])):
                 $month = $_GET['month'];
                 include 'partials/month_articles.php';
@@ -77,9 +65,7 @@ require 'partials/head.php';
             
             ?>
         </main>
-
         <aside>
             <?php require 'partials/aside.php'; ?>
         </aside>
-
 <?php require 'partials/footer.php'; ?>
