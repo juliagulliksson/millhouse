464709fc569ef527cf9ca8ce8385e49ba69be522
vctrklndr <victor@nasa.se>
2017-11-21 12:50:10 +0100

    Changed pagination_links

--- partials/pagination_links.php
+++ partials/pagination_links.php
@@ -1,7 +1,9 @@
 <div class="page_number">
     <a href="index.php?page=1">HOME</a>
-    <a href="index.php?page=<?= $page - 1 ?>">&laquo;</a>
     <?php 
+    if($page -1 >= 1){ ?> 
+    <a href="index.php?page=<?= $page - 1 ?>">&laquo;</a>
+    <?php }
     if($page -1 >= 1){ ?>       
         <a href="index.php?page=<?= $page - 1 ?>"><?= $page - 1; } ?></a>
         <a id="current_page" href="index.php?page=<?= $page ?>"><?= $page ?></a>
@@ -14,10 +16,12 @@
         <?php  } 
     if($page + 2 < $last_page ){ ?>
         <a href="index.php?page=<?= $page + 3 ?>"><?= $page + 3?></a>
-        <?php  } ?>
-                  
-    <a href="index.php?page=<?= $page + 1 ?>">&raquo;</a>
-    <?php if($last_page > 25) { ?>
-    <a href="index.php?page=<?= $last_page ?>">LAST PAGE</a> 
+        <?php  } 
+    
+    if($page < $last_page ){ ?>             
+        <a href="index.php?page=<?= $page + 1 ?>">&raquo;</a>
+    <?php }
+    if($last_page > 25) { ?>
+        <a href="index.php?page=<?= $last_page ?>">LAST PAGE</a> 
     <?php } ?>
 </div>
