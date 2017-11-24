4e2bbc9056c344a1ca6cb1572fc315144128ac18
andxz <andrez.romero@hotmail.com>
2017-11-24 11:14:57 +0100

    validering

--- partials/register.php
+++ partials/register.php
@@ -1,9 +1,60 @@
 <?php
     //If fields for register is filled
-    if (!empty($_POST["register_username"]) &&
-        !empty($_POST["register_password"]) &&
-        !empty($_POST["register_email"])){
+if(!empty($_POST["register-user"])) {
+
+
+    $user = trim($_POST["register_username"]);
+    $pass = trim($_POST["register_password"]);
+    $pass_verify = trim($_POST['verify_password']);
+    $mail = trim($_POST["register_email"]);
+    
+
+/* Form Required Field Validation */
+foreach($_POST as $key=>$value) {
+	if(empty($_POST[$key])) {
+	$error_message = "All Fields are required";
+	break;
+	}
+}
+
+
+/* No Withspaces */
+
+
+	if(!isset($error_message)) {
+    if(preg_match('/\s/',$user) ) {
+		$error_message = "Whitespace not allowed in Username.";
+		}
+        }
+
+	if(!isset($error_message)) {
+    if(preg_match('/\s/',$pass) ) {
+		$error_message = "Whitespace not allowed in Password.";
+		}
+        }
+
+
+	/* Username Validation */
+	if(!isset($error_message)) {
+		if (strlen($user) > 20) {
+		$error_message = "Maximum is 20 characters";
+		}
+        }
+
+	/* Password Matching Validation */
+	if($pass != $pass_verify){ 
+	$error_message = 'Passwords should be same<br>'; 
+	}
+
+	/* Email Validation */
+	if(!isset($error_message)) {
+		if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+		$error_message = "Invalid Email Address";
+		}
+        }
         
+	if(!isset($error_message)) {
+
         $new_username = $_POST["register_username"];     
         $new_email = $_POST["register_email"];        
         $new_password = password_hash($_POST["register_password"], PASSWORD_DEFAULT);     
@@ -42,4 +93,6 @@
             $new_email,
             $admin);                 
         }}
+        }
+
 ?>
