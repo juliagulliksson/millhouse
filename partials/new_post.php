405c9295515e1bb18152caedd4df9c41c0f84eed
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-11-28 13:07:38 +0100

    Working on image upload error

--- partials/new_post.php
+++ partials/new_post.php
@@ -7,9 +7,28 @@
     <?php 
     if(isset($_GET['newpost'], $_GET['error'])) { ?>
         <p class="error-message">Error: All fields are required for submission.</p>
-   <?php } ?>
+   <?php }
+   //check if alt-text is empty when image is not empty 
+   if(isset($_POST['newpost_submit'])):
+        if(!empty($_FILES['image']) && empty($_POST['alt_text'])): ?>
+            <p class="error-message">Error: Enter an image text</p>
+        <?php
+        endif;
+   endif;
+   if(isset($_POST['newpost_submit'])):
+     // If blog post with image is posted
+     if (!empty($_FILES["image"]) && !empty($_POST["alt_text"])):
+        require "actions/upload_image_sql.php";
+        if(is_array($upload_ok) && !empty($upload_ok)):
+            foreach ($upload_ok as $error_message):?>
+            <p class="error-message"><?= $error_message ?><br/></p>
+            <?php endforeach;
+            endif;
+        endif;
+    endif; ?>
+
     <div class="center-heading"><h1>Write a new blog post</h1></div>
-    <form action="actions/insert_blogpost_sql.php" method="POST"
+    <form action="profile.php?newpost=true" method="POST"
        enctype="multipart/form-data">
         <div class="form-group">
             <div class="form-group__title">
@@ -46,7 +65,7 @@
             <input type="text" name="alt_text" placeholder="Write something about your image..">
         </div><!--/.form-group__category-collapse-->
     </div><!--/.form-group-collapse-->
-    <input type="submit" name="submit" value="Submit">
+    <input type="submit" name="newpost_submit" value="Submit">
     </form>
 </div>
 <!-- /.insert-form-collapse -->
