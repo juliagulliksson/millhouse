25d7d0382dc123efd8850d8273572d72dc0cad35
vctrklndr <victor@nasa.se>
2017-12-04 22:07:33 +0100

    Updated about page

--- about.php
+++ about.php
@@ -4,55 +4,60 @@ require 'partials/head.php';
 ?>
 
 <div class="about-container">
-    <img src="images/office.jpg" alt="Office" />
+    <!-- <img src="images/office.jpg" alt="Office" /> -->
     <h1 id="about">About Millhouse</h1>
-    <p class="about-text">
-        Based in the heart of Stockholm, Millhouse brings you some of the finest
-        fashion items of the world.
+    <p>
+        Based in the heart of Stockholm, Millhouse brings you some of the finest fashion<br />
+        items of the world.
     </p>
-    <p class="about-text">
-        At Millhouse you find elegant watches and sunglasses that combine high
-        quality and fashion. We want your look to be both modern and timeless
-        at the same time. The right accessories brings out the best in your
-        personal style and make you the center of the party. 
+    <p>
+        At Millhouse you find elegant watches and sunglasses that combine high quality and<br />
+        fashion. We want your look to be both modern and timeless at the same time.<br />
+        The right accessories brings out the best in your personal style and make<br />
+        you the center of the party. 
     </p>
-    <p class="about-text">
-        Millhouse interior combines tasteful Swedish design with a unique and
+    <p>
+        Millhouse interior combines tasteful Swedish design with a unique and<br />
         luxurious twist. Making your home the talk of the town (in a good way).
     </p>
-    <h2 id="press">Press</h2>
-    <p class="about-text">
-        Massa vitae tortor condimentum lacinia quis vel eros donec. Pulvinar pellentesque
-        habitant morbi tristique senectus et netus. Arcu felis bibendum ut tristique et
-        egestas. Urna id volutpat lacus laoreet non curabitur. Viverra nam libero justo
-        laoreet sit. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. At
-        quis risus sed vulputate odio. Sollicitudin ac orci phasellus egestas tellus rutrum
-        tellus. Aliquam sem et tortor consequat.
-    </p>
-    <h2 id="sustainability">Sustainability</h2>
-    <p class="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Sapien eget mi proin sed
-        libero enim sed. Tellus cras adipiscing enim eu turpis egestas. Sapien eget mi proin
-        sed libero enim sed. Tempor id eu nisl nunc mi ipsum. Vitae aliquet nec ullamcorper
-        sit amet risus. Sed velit dignissim sodales ut. Consequat mauris nunc congue nisi
-        vitae suscipit tellus. Tincidunt dui ut ornare lectus. Hac habitasse platea dictumst
-        quisque sagittis purus sit amet.
-    </p>
-
-    <h2 id="investors">Investors</h2>
-    <p class="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas
-        erat imperdiet sed euismod nisi. Pretium quam vulputate dignissim suspendisse in
-        est ante in nibh. Bibendum enim facilisis gravida neque convallis a cras semper auctor.
-    </p>
-    <h2 id="jobs">Jobs</h2>
-    <p>Want to work with us at Millhouse?
-    <br />We always look for new colleagues here at Millhouse. Please send us your CV!
-    </p>
+    <h3>"We're all working together;<br />that's the secret"</h3>
+    <div class="about-group">
+        <div class="press">
+            <h2 id="press">Press</h2>
+            <p class="about-text">
+                Massa vitae tortor condimentum lacinia quis vel eros donec. Pulvinar pellentesque
+                habitant morbi tristique senectus et netus.
+            </p> 
+        </div>
+        <div class="sustainability">
+            <h2 id="sustainability">Sustainability</h2>
+            <p class="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+    </div>
+    <div class="about-group">
+        <div class="investors">
+            <h2 id="investors">Investors</h2>
+            <p class="about-text">Est placerat in egestas
+                erat imperdiet sed euismod nisi. Pretium quam vulputate dignissim suspendisse in
+                est ante in nibh.
+            </p>
+        </div>
+        <div class="jobs">
+            <h2 id="jobs">Jobs</h2>
+            <p>Want to work with us at Millhouse?<br />
+                We always look for new colleagues here at Millhouse. Please send us your CV!
+            </p>
+        </div>
+    </div>
     <h2 id="contact">Contact</h2>
     <p>Millhouse AB
         <br />Birger Jarlsgatan 63
         <br />113 56 Stockholm
+    </p>
+    <p>+46(0) 8 123 666 333
+        <br /><a href="mailto:info@millhouse.se">info@millhouse.se</a>
     </p>
 </div>
 
--- partials/footer.php
+++ partials/footer.php
@@ -54,6 +54,5 @@
             integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
             crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/tota11y@0.1.6/build/tota11y.min.js"></script>
 </body>
 </html>
