ead2252bab26a5d9ebd031fc1bc3c18794f17c62
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2017-12-07 16:43:31 +0100

    Fixed Save profile settings

--- profile_includes/edit_profile.php
+++ profile_includes/edit_profile.php
@@ -18,6 +18,10 @@ if(empty($_POST['new_email'])){
     $error_messages[] = 'The e-mail can\'t be empty';
     $update_ok = false;
 }
+if($new_username == $existing_username && $new_email == $existing_email
+&& empty($_FILES['new_profile_picture']['name'])){
+    $error_messages[] = 'No changes where made';
+}
 
 require 'partials/functions/check_user_values.php';
 require "partials/functions/check_if_duplicate.php";
@@ -44,17 +48,20 @@ if($new_email != $existing_email){
 
 $check_user_input = check_user_values($new_username, $new_email);
 
-
 //Uploads profile picture and/or username, and/or email
-if(!empty($_FILES['new_profile_picture']['name']) 
-&& gettype($check_user_input) == 'boolean' &&
-$update_ok){
+if(!empty($_FILES['new_profile_picture']['name']) && 
+    gettype($check_user_input) == 'boolean' &&
+    $update_ok && 
+    empty($error_messages))
+{
     require "profile_includes/profile_actions/update_profile_pic_sql.php";
 }
 //Updates profile with new email and/or username, if profile picture is empty
-elseif(empty($_FILES['new_profile_picture']['name']) 
-&& gettype($check_user_input) == 'boolean' &&
-    $update_ok){
+elseif(empty($_FILES['new_profile_picture']['name']) && 
+    gettype($check_user_input) == 'boolean' &&
+    $update_ok && 
+    empty($error_messages))
+{
     require "profile_includes/profile_actions/update_profile_sql.php";
 }
 
